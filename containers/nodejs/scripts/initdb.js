461b2ff04592ce930cec2006bb7a01a2e47de5b4
Colin Chan <colin.th.chan@gmail.com>
2023-05-05 15:52:18 -0400

    Add initdb script for project startup

---
+++ containers/nodejs/scripts/initdb.js
@@ -0,0 +1,36 @@
+const nano = require('nano')('http://admin:password@couchdb:5984');
+
+const DB_NAME = 'testdb';
+let db;
+
+async function createDB() {
+  try {
+    await nano.db.create(DB_NAME);
+    db = nano.use(DB_NAME);
+    console.log(`Database '${DB_NAME}' created successfully.`);
+  } catch (error) {
+    if (error.statusCode === 412) {
+      db = nano.use(DB_NAME);
+      console.log(`Database '${DB_NAME}' already exists.`);
+    } else {
+      console.error(`Error creating database '${DB_NAME}':`, error.message);
+    }
+  }
+};
+
+async function initDB() {
+  try {
+    await createDB();
+    const response = await db.insert({
+         _id: 'some_uuid',
+         username: 'admin',
+         password: 'password',
+         email: 'admin@foobar.com'
+    });
+    console.log(`Document insert response: ${response}`);
+  } catch (error) {
+    console.error(`Error inserting document`, error.message);
+  }
+};
+
+initDB();
